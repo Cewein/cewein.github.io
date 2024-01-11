a27ece40809156fcdd422a9eaa420026f96fe729
Maximilien Nowak <pro.mnowak@gmail.com>
2024-01-11 12:49:28 +0100

    Update htmlGen.js
    data gen is working, now we need to populate, design and animate everything

--- js/htmlGen.js
+++ js/htmlGen.js
@@ -13,20 +13,29 @@ async function loadData() {
 // Define a synchronous function named "generate"
 function generate() {
     var jsonData = null; // Declare a variable to store the loaded JSON data
+    divGen = document.getElementById("generator");
 
     // Call the asynchronous "loadData" function and use the "then" method to handle the resolved value
     // The "data" parameter represents the resolved value (loaded JSON data) of the promise
     loadData().then((data) => {
         jsonData = data;
-        jsonData.data.forEach(r => {
+        jsonData.data.forEach(j => {
 
-            console.log(r);
+            var htmldata = 
+            `<div class="p-1 row align-items-md-stretch">`+
+            `    <div class="col-md">`+
+            `        <div class="h-100 p-5 text-bg-dark">`+
+            `            <h2>${j.title}</h2>`+
+            `            <p>${j.abstract}</p>`+
+            `            <button class="btn btn-outline-light" type="button">Example button</button>`+
+            `        </div>`+
+            `    </div>`+
+            `</div>`;
+            var e = document.createElement(`div`);
+            e.innerHTML = htmldata;
 
+            divGen.appendChild(e);
+            
         });
     });
-
-    var e = document.createElement('div');
-    //e.innerHTML = htmldata;
-
-    divGen = document.getElementById("generator")
 }
