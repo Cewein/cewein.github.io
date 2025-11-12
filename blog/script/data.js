cd45d32175efbae881b191da7da2f23423a959f2
Maximilien Nowak <pro.mnowak@gmail.com>
2025-11-12 16:13:32 +0100

    added comparaison picture

---
+++ blog/script/data.js
@@ -0,0 +1,42 @@
+var data = {
+    "imageBoxes": [    
+        {
+            "title": "",
+            "elements": [
+                {
+                    "title": "Mesh Pruning",
+                    "elements": [                                                                                               
+                        {
+                            "image": "img/iter_3000_Rendered_RGB_43.png",
+                            "title": "Default Pruning",
+                        },
+                        {
+                            "image": "img/iter_3000_Rendered_RGB_43_m.png",
+                            "title": "Mesh-Aware Pruning",
+                        },
+                        {
+                            "image": "img/iter_3000_Rendered_Normals_43.png",
+                            "title": "Default Normal",
+                        },
+                        {
+                            "image": "img/iter_3000_Rendered_Normals_43_m.png",
+                            "title": "Mesh-Aware Normal",
+                        },
+                        {
+                            "image": "img/iter_3000_RGB_L1_Diff_43.png",
+                            "title": "Default diff",
+                        },
+                        {
+                            "image": "img/iter_3000_RGB_L1_Diff_43_m.png",
+                            "title": "Mesh-Aware diff",
+                        },
+                        {
+                            "image": "img/iter_3000_GT_RGB_43.png", 
+                            "title": "Reference", 
+                        },      
+                    ]
+                },
+            ]
+        },    
+    ]
+}
